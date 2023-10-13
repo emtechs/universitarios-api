3a0bed023c07e67de195b367cb1791c039cfe670
Lucas Soares <lucassoares2007@hotmail.com>
2023-10-12 21:50:18 -0300

    refactor: documents

--- src/services/users/documentsUser.service.ts
+++ src/services/users/documentsUser.service.ts
@@ -1,6 +1,9 @@
 import { prisma } from '../../lib'
 
 export const documentsUserService = async (id: string, record_id: string) => {
+  let action_ft = {}
+  let action_mt = {}
+
   const [documentFt, documentMat] = await Promise.all([
     prisma.documentUser.findFirst({
       where: { user_id: id, document: { category: 'FT' } },
@@ -10,6 +13,17 @@ export const documentsUserService = async (id: string, record_id: string) => {
             id: true,
             status: true,
             image: { select: { id: true, url: true } },
+            actions: {
+              take: 1,
+              select: {
+                id: true,
+                description: true,
+                justification: true,
+                created_at: true,
+                user: { select: { id: true, name: true, cpf: true } },
+              },
+              orderBy: { created_at: 'desc' },
+            },
           },
         },
       },
@@ -22,14 +36,28 @@ export const documentsUserService = async (id: string, record_id: string) => {
             id: true,
             status: true,
             image: { select: { id: true, url: true } },
+            actions: {
+              take: 1,
+              select: {
+                id: true,
+                description: true,
+                justification: true,
+                created_at: true,
+                user: { select: { id: true, name: true, cpf: true } },
+              },
+              orderBy: { created_at: 'desc' },
+            },
           },
         },
       },
     }),
   ])
 
+  if (documentFt?.document.actions) action_ft = documentFt.document.actions[0]
+  if (documentMat?.document.actions) action_mt = documentMat.document.actions[0]
+
   return {
-    foto: { ...documentFt?.document },
-    matricula: { ...documentMat?.document },
+    foto: { ...documentFt?.document, action: action_ft },
+    matricula: { ...documentMat?.document, action: action_mt },
   }
 }
