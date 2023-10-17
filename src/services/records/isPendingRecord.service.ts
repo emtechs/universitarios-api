ce85384de8917e57b2b19ab2b7918b0e4d15caf3
Lucas Soares <lucassoares2007@hotmail.com>
2023-10-17 03:40:27 -0300

    refactor: pending

--- src/services/records/isPendingRecord.service.ts
+++ src/services/records/isPendingRecord.service.ts
@@ -1,8 +1,11 @@
 import { AppError } from '../../errors'
 import { prisma } from '../../lib'
-import { isPendingUserService } from '../users'
+import { isPendingDocument } from '../../scripts'
 
 export const isPendingRecordService = async (record_id: string) => {
+  let is_pending = false
+  let status = 'CONFIRMED'
+
   const record = await prisma.record.findUnique({
     where: { key: record_id },
     select: { user_id: true },
@@ -10,5 +13,34 @@ export const isPendingRecordService = async (record_id: string) => {
 
   if (!record) throw new AppError('record not found', 404)
 
-  return await isPendingUserService(record.user_id, record_id, true)
+  const id = record.user_id
+
+  const [documentFt, documentMat, documentDoc, documentDocBack, documentEnd] =
+    await Promise.all([
+      isPendingDocument(id, 'FT'),
+      isPendingDocument(id, 'MAT', record_id),
+      isPendingDocument(id, 'DOC'),
+      isPendingDocument(id, 'DOC', undefined, true),
+      isPendingDocument(id, 'END'),
+    ])
+
+  if (
+    documentFt?.document.status === 'RECEIVED' ||
+    documentMat?.document.status === 'RECEIVED' ||
+    documentDoc?.document.status === 'RECEIVED' ||
+    documentDocBack?.document.status === 'RECEIVED' ||
+    documentEnd?.document.status === 'RECEIVED'
+  )
+    is_pending = true
+
+  if (
+    documentFt?.document.status === 'REFUSED' ||
+    documentMat?.document.status === 'REFUSED' ||
+    documentDoc?.document.status === 'REFUSED' ||
+    documentDocBack?.document.status === 'REFUSED' ||
+    documentEnd?.document.status === 'REFUSED'
+  )
+    status = 'PENDING'
+
+  return { is_pending, status }
 }
--- src/services/users/isPendingUser.service.ts
+++ src/services/users/isPendingUser.service.ts
@@ -1,10 +1,6 @@
 import { isPendingDocument } from '../../scripts'
 
-export const isPendingUserService = async (
-  id: string,
-  record_id: string,
-  is_record?: boolean,
-) => {
+export const isPendingUserService = async (id: string, record_id: string) => {
   const [documentFt, documentMat, documentDoc, documentDocBack, documentEnd] =
     await Promise.all([
       isPendingDocument(id, 'FT'),
@@ -22,17 +18,6 @@ export const isPendingUserService = async (
     !documentEnd
   )
     return true
-
-  if (is_record) {
-    if (
-      documentFt.document.status === 'RECEIVED' ||
-      documentMat.document.status === 'RECEIVED' ||
-      documentDoc.document.status === 'RECEIVED' ||
-      documentDocBack.document.status === 'RECEIVED' ||
-      documentEnd.document.status === 'RECEIVED'
-    )
-      return true
-  }
 
   if (
     documentFt.document.status === 'REFUSED' ||
